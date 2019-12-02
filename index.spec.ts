94da24b4a491e9c4bddc08ed953257188da610ac
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2019-12-02 18:21:10 +0100

    minor: add unit test for standalone deployment

---
+++ index.spec.ts
@@ -0,0 +1,41 @@
+import {MongoClient, ObjectId} from 'mongodb';
+import {deleteAll, drop, init, load, MongoURL} from "./index";
+
+const mockData = {
+    'device-groups':
+        [
+            {
+                '_id': new ObjectId('0123456789abcdef01234560'),
+                'devices': [],
+                'lastModified': '1572945305311',
+            },
+            {
+                '_id': new ObjectId('0123456789abcdef01234561'),
+                'parent': '0123456789abcdef01234560',
+                'devices': [],
+                'lastModified': '1572945305311',
+            },
+        ],
+};
+
+const doStandardTesting = (url: Promise<MongoURL>) => {
+    let client: MongoClient;
+    return url
+        .then(url => MongoClient.connect(url.shortUrl))
+        .then(_client => client =_client)
+        .then(() => load(mockData))
+        .then(() => client.db('test').collections())
+        .then(cols =>
+            cols.filter(col => col.collectionName.includes('device-group'))
+                .forEach(col => col.countDocuments().then(count => expect(count).toBe(2))))
+        .then(deleteAll)
+        .then(() => client.db('test').collections())
+        .then(cols => cols.forEach(col => col.countDocuments().then(count => expect(count).toBe(0))))
+        .then(drop)
+        .catch(fail)
+};
+
+describe('MongoCI', () => {
+    it('standalone instance with no credentials', done => doStandardTesting(init()).finally(done));
+});
+
